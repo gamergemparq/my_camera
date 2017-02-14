18c594ae6c3194f43e7a9ba9ab69aed0dde92abb
Bård Farstad <bard@snowball.digital>
2017-02-14 16:26:44 +0100

    - Added example for server upload of the picture taken.

--- index.ios.js
+++ index.ios.js
@@ -16,6 +16,8 @@ import {
 
 import Camera from 'react-native-camera';
 
+const PicturePath = "";
+
 export default class MyCamera extends Component {
   render() {
     return (
@@ -25,18 +27,55 @@ export default class MyCamera extends Component {
              this.camera = cam;
            }}
            style={styles.preview}
-           aspect={Camera.constants.Aspect.fill}>
+           aspect={Camera.constants.Aspect.fill}
+           captureTarget={Camera.constants.CaptureTarget.disk}>
            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+           <Text style={styles.capture} onPress={this.storePicture.bind(this)}>[UPLOAD]</Text>
+
        </Camera>
       </View>
     );
   }
 
+  storePicture(){
+      console.log( PicturePath );
+      if (PicturePath) {
+        // Create the form data object
+        var data = new FormData();
+        data.append('picture', {uri: PicturePath, name: 'selfie.jpg', type: 'image/jpg'});
+
+        // Create the config object for the POST
+        // You typically have an OAuth2 token that you use for authentication
+        const config = {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'multipart/form-data;',
+           'Authorization': 'Bearer ' + 'SECRET_OAUTH2_TOKEN_IF_AUTH',
+         },
+         body: data,
+        }
+
+        fetch("https://postman-echo.com/post", config)
+         .then((responseData) => {
+             // Log the response form the server
+             // Here we get what we sent to Postman back
+             console.log(responseData);
+         })
+         .catch(err => {
+           console.log(err);
+         })
+    }
+  }
+
   takePicture() {
    this.camera.capture()
-     .then((data) => console.log(data))
+     .then((data) => {
+         console.log(data);
+         PicturePath = data.path;
+     })
      .catch(err => console.error(err));
- }
+  }
 }
 
 const styles = StyleSheet.create({
